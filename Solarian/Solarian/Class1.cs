db64303e2c2bfe85053e9f9fc9fc2fee8c7b6a31
Ilya Kulikov <Ilya.Kulikov@comindware.ru>
2022-12-29 23:52:58 +0300

    Add AutoCad and SQLite

--- Solarian/Solarian/Class1.cs
+++ Solarian/Solarian/Class1.cs
@@ -1,7 +1,61 @@
-﻿namespace Solarian
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+
+[assembly: CommandClass(typeof(MyFirstProject1.Class1))]
+
+namespace MyFirstProject1
 {
     public class Class1
     {
+        [CommandMethod("AdskGreeting")]
+        public void AdskGreeting()
+        {
+            // Get the current document and database, and start a transaction
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
 
+            // Starts a new transaction with the Transaction Manager
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Open the Block table record for read
+                BlockTable acBlkTbl;
+                acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,
+                                             OpenMode.ForRead) as BlockTable;
+
+                // Open the Block table record Model space for write
+                BlockTableRecord acBlkTblRec;
+                acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace],
+                                                OpenMode.ForWrite) as BlockTableRecord;
+
+                /* Creates a new MText object and assigns it a location,
+                text value and text style */
+                using (MText objText = new MText())
+                {
+                    // Specify the insertion point of the MText object
+                    objText.Location = new Autodesk.AutoCAD.Geometry.Point3d(2, 2, 0);
+
+                    // Set the text string for the MText object
+                    objText.Contents = "Greetings, Welcome to AutoCAD .NET";
+
+                    // Set the text style for the MText object
+                    objText.TextStyleId = acCurDb.Textstyle;
+
+                    // Appends the new MText object to model space
+                    acBlkTblRec.AppendEntity(objText);
+
+                    // Appends to new MText object to the active transaction
+                    acTrans.AddNewlyCreatedDBObject(objText, true);
+                }
+
+                // Saves the changes to the database and closes the transaction
+                acTrans.Commit();
+            }
+        }
     }
 }
